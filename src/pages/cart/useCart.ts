85dcf6cc305e6ee3787f4fccc882b2f9f5769096
Nelson Munthe <nnelson.munthe@gmail.com>
2024-11-16 10:45:00 +0700

    purchase order

--- src/pages/cart/useCart.ts
+++ src/pages/cart/useCart.ts
@@ -3,11 +3,15 @@ import { useTypedSelector } from "../../store/store";
 import { onChangeProductQuantity } from "../../store/wishListReducer"
 import { ChangeWishListProps } from "../../interfaces/common";
 import { createPurchase } from "../../apiCall/purchase";
+import { useNavigate } from "react-router-dom";
+import { useSnackbar } from 'notistack'
 
 
 const useCart = () => {
     const dispath = useDispatch()
     const  { products, totalPrice} =  useTypedSelector(state => state.wishList)
+    const navigate = useNavigate()
+    const { enqueueSnackbar } = useSnackbar()
   
     const onChangeNumberProduct = (productId: number, event: React.ChangeEvent<HTMLInputElement>) : void => {
         const payload:ChangeWishListProps = {
@@ -21,6 +25,13 @@ const useCart = () => {
 
         try {
             event.preventDefault();
+            const accessToken = localStorage.getItem('accessToken');
+            if(!accessToken) {
+                navigate('/login');
+                enqueueSnackbar("You are not login yet", {variant: "error"})
+                return
+            }
+
             const response =  await createPurchase(products, totalPrice);
             console.log(response)
         } catch (error) {
